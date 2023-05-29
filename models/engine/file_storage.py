75cffeaa4f8481a1114bbc5210c1163a898da199
rungene <rungenesamuel@gmail.com>
2023-05-29 00:58:43 -0700

    method to retrieve one object and method to count the number of objects in storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,6 +48,33 @@ class FileStorage:
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
+    def get(self, cls, id):
+        """Retrives an objects based on the class and id
+        Args:
+            cls:The class of the object to retrive
+            id: The ID of the object to retive
+
+        Return:
+             the object based on the class and its ID, or
+             None if not found"""
+        for item in self.__objects.values():
+            if item.__class__ == cls and item.id = id:
+                return item
+        return None
+
+    def count(self, cls=None):
+        """count the number of objects in storage
+        Args:
+            cls:class (optional)
+
+        Return:
+             number of objects in storage matching the given class.
+             If no class is passed, returns the count of all objects
+             in storage."""
+        if cls is None:
+            return len(self.all())
+        return len(self.all(cls))
+
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
@@ -55,7 +82,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
