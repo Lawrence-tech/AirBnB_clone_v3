9f46b5b4701cd4a7693e538f03048c45dedbc1e0
rungene <rungenesamuel@gmail.com>
2023-05-29 11:21:24 -0700

    create a file __init__.py

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""__init__ module."""
+from flask import Blueprint
+
+
+# Create a Blueprint instance for the views
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# import views and register them with the Blueprint
+if (__name__ == 'api.v1.views.index'):
+    from api.v1.views.index import *
