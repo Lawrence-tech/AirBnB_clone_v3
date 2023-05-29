1647ae472a827dd329eaf3eef38a8167fd6144b4
rungene <rungenesamuel@gmail.com>
2023-05-29 10:54:17 -0700

    Adding app.py to define flask server

---
+++ api/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import os
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+# Register the blueprint
+app.register_blueprint(app_views)
+
+# Register the teardown handler
+@app.teardown_appcontext
+def teardown_handler(exception=None):
+    """Teardown method to handle storage.close"""
+    storage.close()
+
+if __name__ == "__main__":
+    # set host and port based on environment variables or defaults
+    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.environ.get("HBNB_API_PORT", 5000))
+
+    # Run Flask app with specified port, host
+    app.run(host=host, port=port, threaded=True)
