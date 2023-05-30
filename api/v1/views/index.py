d05e05ce45f51f608807e5aa650ddaf5f0f42983
Lawrence-tech <lawrencesagini2@gmail.com>
2023-05-30 05:08:38 +0300

    some stats endpoint route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,21 @@
 """index module returns a JSON: "status": 'Ok'"""
 from flask import jsonify, Blueprint
 from api.v1.views import app_views
-
+from models import storage
 
 # Define a route /status on app_views blueprint
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """returns a JSON: "status": 'OK'"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def obj_stats():
+    objs = {"amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")}
+    return jsonify(objs)
+
