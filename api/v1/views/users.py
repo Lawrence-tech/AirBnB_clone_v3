69dae3c3238ec94daa51d2bc52238199e79e5148
rungene <rungenesamuel@gmail.com>
2023-05-30 12:15:04 -0700

    Corrections-Module error

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -3,7 +3,7 @@
 from flask import jsonify, Blueprint, request, abort
 from api.v1.views import app_views
 from models import storage
-from models.state import User
+from models.user import User
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
