d5c1a1caca91f67acbf7f8678b356969a29f8d69
rungene <rungenesamuel@gmail.com>
2023-05-30 08:21:16 -0700

    Corrections: Module not found error

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,4 +9,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 # import views and register them with the Blueprint
 if (__name__ == 'api.v1.views'):
     from api.v1.views.index import *
-    from api.v1.views.state import *
+    from api.v1.views.states import *
